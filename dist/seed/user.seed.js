7cdc7814d30b67088aee6418971ce9eba6fdfc91
joseor13 <oswirr13@mail.fresnostate.edu>
2019-03-28 15:08:22 -0700

    first commit

--- dist/seed/user.seed.js
+++ dist/seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "joserr@shift3tech.com",
+  "username": "joserr",
+  "password": "123asd",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
